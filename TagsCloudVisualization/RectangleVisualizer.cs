5ba3a735c2cb6a85ebca4009eae7616522d25b18
Saylala <boulevardoffakes@gmail.com>
2016-10-22 23:17:04 +0500

    Visualiser

---
+++ TagsCloudVisualization/RectangleVisualizer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+
+namespace TagsCloudVisualization
+{
+    class RectangleVisualizer
+    {
+        private readonly Pen pen;
+        private readonly Color backgroundColor;
+        private readonly int imageWidth;
+        private readonly int imageHeight;
+        public RectangleVisualizer(Pen pen, Color backgroundColor, int imageWidth, int imageHeight)
+        {
+            this.pen = pen;
+            this.backgroundColor = backgroundColor;
+            this.imageWidth = imageWidth;
+            this.imageHeight = imageHeight;
+        }
+        public RectangleVisualizer()
+        {
+            pen = new Pen(Color.Yellow, 1);
+            backgroundColor = Color.Black;
+            imageWidth = 1000;
+            imageHeight = 1000;
+        }
+        public void Draw(IEnumerable<System.Drawing.Rectangle> data, Bitmap image)
+        {
+            var graphics = Graphics.FromImage(image);
+            graphics.Clear(backgroundColor);
+            foreach (var sample in data)
+                graphics.DrawRectangle(pen, sample);
+            graphics.Save();
+        }
+
+        public void Visualise(IEnumerable<System.Drawing.Rectangle> data, string filename)
+        {
+            var image = new Bitmap(imageWidth, imageHeight);
+            Draw(data, image);
+            image.Save(filename);
+        }
+    }
+}
